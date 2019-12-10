5e5c966a174dbd14d96ae9aa51ec66f53184663e
unknown <santiagoyacaman@uninorte.edu.co>
2019-12-10 11:07:04 -0500

    LAST_VERSION_10/12/2019_11:06AM

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -9,59 +9,59 @@ import "firebase/firebase-database";
 import Navigation from "../navigation/Navigation";
 
 function Stats(props) {
-  const allBrigadistas = useSelector(state => state.brigada.allBrigades);
+    const allBrigadistas = useSelector(state => state.brigada.allBrigades);
 
-  let orderedArray = allBrigadistas;
-  const compare = (a, b) => {
-    const elementA = a.acceptRatio;
-    const elementB = b.acceptRatio;
+    let orderedArray = allBrigadistas;
+    const compare = (a, b) => {
+        const elementA = a.acceptRatio;
+        const elementB = b.acceptRatio;
 
-    let comparison = 0;
-    if (elementA > elementB) {
-      comparison = 1;
-    } else if (elementA < elementB) {
-      comparison = -1;
-    }
-    return comparison * -1;
-  };
-  orderedArray.sort(compare);
+        let comparison = 0;
+        if (elementA > elementB) {
+            comparison = 1;
+        } else if (elementA < elementB) {
+            comparison = -1;
+        }
+        return comparison * -1;
+    };
+    orderedArray.sort(compare);
 
-  let displayStats = orderedArray.map((item, index) => {
+    let displayStats = orderedArray.map((item, index) => {
+        return (
+            <tr key={index}>
+                <td>
+                    <img src={item.imagen} height="50" width="50" />
+                </td>
+                <td>{item.nombre + " " + item.apellido}</td>
+                <td>{item.accepted}</td>
+                <td>{item.receivedNotif}</td>
+                <td>{(item.acceptRatio * 100).toFixed(2)}</td>
+            </tr>
+        );
+    });
+
+    app.auth().onAuthStateChanged(user => {
+        // Para llevarlo a login window si no está conectado
+        if (!user) {
+            props.history.push("/");
+        }
+    });
+
     return (
-      <tr key={index}>
-        <td>
-          <img src={item.imagen} height="50" width="50" />
-        </td>
-        <td>{item.nombre + " " + item.apellido}</td>
-        <td>{item.accepted}</td>
-        <td>{item.receivedNotif}</td>
-        <td>{(item.acceptRatio * 100).toFixed(2)}</td>
-      </tr>
+        <div>
+            <Navigation />
+            <table className="tableStats">
+                <tr>
+                    <th>Imagen</th>
+                    <th>Brigadista</th>
+                    <th>Aceptados</th>
+                    <th>Totales</th>
+                    <th>Aceptados%</th>
+                </tr>
+                {displayStats}
+            </table>
+        </div>
     );
-  });
-
-  app.auth().onAuthStateChanged(user => {
-    // Para llevarlo a login window si no está conectado
-    if (!user) {
-      props.history.push("/");
-    }
-  });
-
-  return (
-    <div>
-      <Navigation />
-      <table className="tableStats">
-        <tr>
-          <th>Imagen</th>
-          <th>Brigadista</th>
-          <th>Aceptados</th>
-          <th>Totales</th>
-          <th>Aceptados%</th>
-        </tr>
-        {displayStats}
-      </table>
-    </div>
-  );
 }
 
 export default withRouter(Stats);
