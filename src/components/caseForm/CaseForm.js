1adad86fb325456e94d4117066d6a3c12795d3c8
sancar22 <santiagoyacaman@uninorte.edu.co>
2019-12-10 12:31:56 -0500

    Tabs_in_case_form

--- src/components/caseForm/CaseForm.js
+++ src/components/caseForm/CaseForm.js
@@ -1,59 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import createFilterOptions from "react-select-fast-filter-options";
 import firebase from "../../routes/Config";
 import { options, optionsPlace, optionsCod, optionsCategory } from "./Options";
 import {
-    fillPlace,
-    fillCode,
-    fillCategory,
-    fillDescription,
+  fillPlace,
+  fillCode,
+  fillCategory,
+  fillDescription
 } from "../../actions/index";
 import CaseFormC from "./CaseFormC";
+import HelpFormC from "./HelpFormC";
+import HotActiveCase from "./HotActiveCase";
+
+import "./CaseForm.css";
 
 function CaseForm() {
-    const brigadistas = useSelector(state => state.brigada);
-    const fillCase = useSelector(state => state.fillCase);
-    const dispatch = useDispatch();
-    const selectedBrigade = brigadistas.selectedBrigade.map(
-        (brigada, index) => (
-            <li key={index} className="listele">
-                {brigada.nombre + " " + brigada.apellido}
-            </li>
-        )
-    );
+  const brigadistas = useSelector(state => state.brigada);
+  const fillCase = useSelector(state => state.fillCase);
+  const [optionsMenu, setOptionsMenu] = useState("Generar");
+  const dispatch = useDispatch();
+  const selectedBrigade = brigadistas.selectedBrigade.map((brigada, index) => (
+    <li key={index} className="listele">
+      {brigada.nombre + " " + brigada.apellido}
+    </li>
+  ));
 
-    const filterOptions1 = createFilterOptions({ options });
+  const filterOptions1 = createFilterOptions({ options });
 
-    function sendCase() {
-        //Botón para enviar notificaciones
-        firebase.updateCaseBranch(brigadistas.selectedBrigade, fillCase);
-        dispatch(fillPlace(null));
-        dispatch(fillCode(null));
-        dispatch(fillCategory(null));
-        dispatch(fillDescription(""));
-        setTimeout(() => notifExpired(), 10000);
-        firebase.pushNotification(brigadistas.selectedBrigade);
-        firebase.resetSelected();
-    }
+  function sendCase() {
+    //Botón para enviar notificaciones
+    firebase.updateCaseBranch(brigadistas.selectedBrigade, fillCase);
+    dispatch(fillPlace(null));
+    dispatch(fillCode(null));
+    dispatch(fillCategory(null));
+    dispatch(fillDescription(""));
+    setTimeout(() => notifExpired(), 10000);
+    firebase.pushNotification(brigadistas.selectedBrigade);
+    firebase.resetSelected();
+  }
 
-    function notifExpired() {
-        firebase.notifExpired(brigadistas.selectedBrigade);
-        firebase.updateRejectedCases(brigadistas.selectedBrigade);
-    }
+  function notifExpired() {
+    firebase.notifExpired(brigadistas.selectedBrigade);
+    firebase.updateRejectedCases(brigadistas.selectedBrigade);
+  }
 
-    return (
+  return (
+    <div className="allCaseContainer">
+      <div className="caseNav">
+        <button
+          className="optionsCase"
+          style={{
+            backgroundColor: optionsMenu === "Generar" && "rgb(184, 184, 184)"
+          }}
+          onClick={() => setOptionsMenu("Generar")}
+        >
+          Generar Caso
+        </button>
+        <button
+          className="optionsCase"
+          style={{
+            backgroundColor: optionsMenu === "Apoyo" && "rgb(184, 184, 184)"
+          }}
+          onClick={() => setOptionsMenu("Apoyo")}
+        >
+          Apoyo
+        </button>
+        <button
+          className="optionsCase"
+          style={{
+            backgroundColor: optionsMenu === "InfoCaso" && "rgb(184, 184, 184)"
+          }}
+          onClick={() => setOptionsMenu("InfoCaso")}
+        >
+          Info. Casos
+        </button>
+      </div>
+      {optionsMenu === "Generar" ? (
         <CaseFormC
-            selectedBrigade={selectedBrigade}
-            options={options}
-            optionsPlace={optionsPlace}
-            optionsCod={optionsCod}
-            optionsCategory={optionsCategory}
-            fillCase={fillCase}
-            filterOptions1={filterOptions1}
-            sendCase={sendCase}
+          selectedBrigade={selectedBrigade}
+          options={options}
+          optionsPlace={optionsPlace}
+          optionsCod={optionsCod}
+          optionsCategory={optionsCategory}
+          fillCase={fillCase}
+          filterOptions1={filterOptions1}
+          sendCase={sendCase}
         />
-    );
+      ) : optionsMenu === "Apoyo" ? (
+        <HelpFormC />
+      ) : (
+        <HotActiveCase />
+      )}
+    </div>
+  );
 }
 
 export default CaseForm;
---
+++ src/components/caseForm/HelpFormC.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HelpFormC(props) {
+  return <div>Help Form</div>;
+}
+
+export default HelpFormC;
