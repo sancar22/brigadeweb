37047f05d38713bd099dd48bbe23cc2cd92f055e
unknown <santiagoyacaman@uninorte.edu.co>
2019-12-13 01:29:22 -0500

    PAIRED_WITH_PHONE_ACTIVE_CASES_DISPLAY_WITH_BUTTONS

--- src/components/caseForm/CaseForm.js
+++ src/components/caseForm/CaseForm.js
@@ -4,11 +4,11 @@ import createFilterOptions from "react-select-fast-filter-options";
 import firebase from "../../routes/Config";
 import { options, optionsPlace, optionsCod, optionsCategory } from "./Options";
 import {
-  fillPlace,
-  fillCode,
-  fillCategory,
-  fillDescription,
-  helpBrigade
+    fillPlace,
+    fillCode,
+    fillCategory,
+    fillDescription,
+    helpBrigade,
 } from "../../actions/index";
 import CaseFormC from "./CaseFormC";
 import HelpFormC from "./HelpFormC";
@@ -16,126 +16,201 @@ import HotActiveCase from "./HotActiveCase";
 import CustomToast from "../custom-toast";
 import "./CaseForm.css";
 import { toast } from "react-toastify";
+import { Minimatch } from "minimatch";
 
 function CaseForm() {
-  const brigadistas = useSelector(state => state.brigada);
-  const fillCase = useSelector(state => state.fillCase);
-  const [optionsMenu, setOptionsMenu] = useState("Generar");
-  const dispatch = useDispatch();
-  const selectedBrigade = brigadistas.selectedBrigade.map((brigada, index) => (
-    <li key={index} className="listele">
-      {brigada.nombre + " " + brigada.apellido}
-    </li>
-  ));
-  const helpCaseFilter = brigadistas.brigadeListOnline
-    .filter(brigade => {
-      return brigade.ocupado === true;
-    })
-    .map((brigade, index) => {
-      return {
-        value: index.toString(),
-        label: `${brigade.nombre + " " + brigade.apellido}`
-      };
-    });
-
-  const filterOptions1 = createFilterOptions({ options });
-  const checkFunctionCase = (place, code, category, description) => {
-    place === null
-      ? toast(<CustomToast title="¡Seleccione Lugar!" />)
-      : code === null
-      ? toast(<CustomToast title="¡Seleccione Código!" />)
-      : category === null
-      ? toast(<CustomToast title="¡Seleccione Categoría!" />)
-      : description === null || description === ""
-      ? toast(<CustomToast title="¡Descripción es obligatoria!" />)
-      : brigadistas.selectedBrigade.length > 1
-      ? toast(<CustomToast title="¡No puede seleccionar a más de uno!" />)
-      : brigadistas.selectedBrigade.length < 1
-      ? toast(<CustomToast title="¡Seleccione a un brigadista!" />)
-      : sendCase();
-  };
-
-  const checkFunctionHelp = brigApoyados => {
-    brigApoyados === null
-      ? toast(<CustomToast title="¡Seleccione brigadista a apoyar!" />)
-      : brigadistas.selectedBrigade.length < 1
-      ? toast(<CustomToast title="¡Seleccione a un brigadista de apoyo!" />)
-      : brigadistas.selectedBrigade.length > 3
-      ? toast(
-          <CustomToast title="¡Solo puede seleccionar a máximo 3 brigadistas de apoyo!" />
+    const brigadistas = useSelector(state => state.brigada);
+    const fillCase = useSelector(state => state.fillCase);
+    const [optionsMenu, setOptionsMenu] = useState("InfoCaso");
+    const dispatch = useDispatch();
+    const selectedBrigade = brigadistas.selectedBrigade.map(
+        (brigada, index) => (
+            <li key={index} className="listele">
+                {brigada.nombre + " " + brigada.apellido}
+            </li>
         )
-      : sendHelp();
-  };
+    );
+    const helpCaseFilter = brigadistas.brigadeListOnline
+        .filter(brigade => {
+            return brigade.ocupado === true;
+        })
+        .map((brigade, index) => {
+            return {
+                value: index.toString(),
+                label: `${brigade.nombre + " " + brigade.apellido}`,
+            };
+        });
 
-  const sendHelp = () => {};
+    const handleButtonClickExt = caso => {
+        firebase.changeSeenExt(caso);
+        const objeto = "Extintor";
+        firebase.miniPushNotification(caso.Expotoken, objeto);
+    };
+    const handleButtonClickCam = caso => {
+        firebase.changeSeenCam(caso);
+        const objeto = "Camilla";
+        firebase.miniPushNotification(caso.Expotoken, objeto);
+    };
+    const activeCases = brigadistas.activeCases.map((caso, index) => (
+        <div className="activeCaseLoop">
+            <div style={{ textAlign: "center" }}>{caso.inicioFecha}</div>
+            <div className="rowCase">
+                <div>{caso.nombre + " " + caso.apellido}</div>
+                <div>{caso.lugar}</div>
+            </div>
+            <div className="rowCase">
+                <div>Código: {caso.codigo}</div>
+                <div>Categoría: {caso.categoria}</div>
+            </div>
+            <br />
+            <div className="requests">
+                <div style={{ textAlign: "center" }}>Apoyos solicitados</div>
+                <div className="requestsContainer">
+                    <div className="objects">
+                        <div style={{ width: "100%", textAlign: "center" }}>
+                            Objetos
+                        </div>
+                        {caso.camilla && (
+                            <button
+                                onClick={() => handleButtonClickCam(caso)}
+                                style={{
+                                    backgroundColor:
+                                        caso.camillaCheck && "green",
+                                }}
+                                className="buttonObj"
+                            >
+                                Camilla
+                            </button>
+                        )}
+                        {caso.extintor && (
+                            <button
+                                onClick={() => handleButtonClickExt(caso)}
+                                style={{
+                                    backgroundColor:
+                                        caso.extintorCheck && "green",
+                                }}
+                                className="buttonObj"
+                            >
+                                Extintor
+                            </button>
+                        )}
+                        {caso.apoyo && (
+                            <button className="buttonObj">Apoyo</button>
+                        )}
+                    </div>
+                    <div className="listApoyo">
+                        <div style={{ width: "100%", textAlign: "center" }}>
+                            Brigadistas
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ));
 
-  function sendCase() {
-    //Botón para enviar notificaciones
+    const filterOptions1 = createFilterOptions({ options });
+    const checkFunctionCase = (place, code, category, description) => {
+        place === null
+            ? toast(<CustomToast title="¡Seleccione Lugar!" />)
+            : code === null
+            ? toast(<CustomToast title="¡Seleccione Código!" />)
+            : category === null
+            ? toast(<CustomToast title="¡Seleccione Categoría!" />)
+            : description === null || description === ""
+            ? toast(<CustomToast title="¡Descripción es obligatoria!" />)
+            : brigadistas.selectedBrigade.length > 1
+            ? toast(<CustomToast title="¡No puede seleccionar a más de uno!" />)
+            : brigadistas.selectedBrigade.length < 1
+            ? toast(<CustomToast title="¡Seleccione a un brigadista!" />)
+            : sendCase();
+    };
 
-    firebase.pushNotification(brigadistas.selectedBrigade, fillCase);
-    dispatch(fillPlace(null));
-    dispatch(fillCode(null));
-    dispatch(fillCategory(null));
-    dispatch(fillDescription(""));
+    const checkFunctionHelp = brigApoyados => {
+        brigApoyados === null
+            ? toast(<CustomToast title="¡Seleccione brigadista a apoyar!" />)
+            : brigadistas.selectedBrigade.length < 1
+            ? toast(
+                  <CustomToast title="¡Seleccione a un brigadista de apoyo!" />
+              )
+            : brigadistas.selectedBrigade.length > 3
+            ? toast(
+                  <CustomToast title="¡Solo puede seleccionar a máximo 3 brigadistas de apoyo!" />
+              )
+            : sendHelp();
+    };
 
-    firebase.resetSelected();
-  }
+    const sendHelp = () => {};
 
-  return (
-    <div className="allCaseContainer">
-      <div className="caseNav">
-        <button
-          className="optionsCase"
-          style={{
-            backgroundColor: optionsMenu === "Generar" && "rgb(184, 184, 184)"
-          }}
-          onClick={() => setOptionsMenu("Generar")}
-        >
-          Generar Caso
-        </button>
-        <button
-          className="optionsCase"
-          style={{
-            backgroundColor: optionsMenu === "Apoyo" && "rgb(184, 184, 184)"
-          }}
-          onClick={() => setOptionsMenu("Apoyo")}
-        >
-          Apoyo
-        </button>
-        <button
-          className="optionsCase"
-          style={{
-            backgroundColor: optionsMenu === "InfoCaso" && "rgb(184, 184, 184)"
-          }}
-          onClick={() => setOptionsMenu("InfoCaso")}
-        >
-          Info. Casos
-        </button>
-      </div>
-      {optionsMenu === "Generar" ? (
-        <CaseFormC
-          selectedBrigade={selectedBrigade}
-          options={options}
-          optionsPlace={optionsPlace}
-          optionsCod={optionsCod}
-          optionsCategory={optionsCategory}
-          fillCase={fillCase}
-          filterOptions1={filterOptions1}
-          checkFunctionCase={checkFunctionCase}
-        />
-      ) : optionsMenu === "Apoyo" ? (
-        <HelpFormC
-          selectedBrigade={selectedBrigade}
-          helpCaseFilter={helpCaseFilter}
-          fillCase={fillCase}
-          filterOptions1={filterOptions1}
-          checkFunctionHelp={checkFunctionHelp}
-        />
-      ) : (
-        <HotActiveCase />
-      )}
-    </div>
-  );
+    function sendCase() {
+        //Botón para enviar notificaciones
+
+        firebase.pushNotification(brigadistas.selectedBrigade, fillCase);
+        dispatch(fillPlace(null));
+        dispatch(fillCode(null));
+        dispatch(fillCategory(null));
+        dispatch(fillDescription(""));
+
+        firebase.resetSelected();
+    }
+
+    return (
+        <div className="allCaseContainer">
+            <div className="caseNav">
+                <button
+                    className="optionsCase"
+                    style={{
+                        backgroundColor:
+                            optionsMenu === "InfoCaso" && "rgb(184, 184, 184)",
+                    }}
+                    onClick={() => setOptionsMenu("InfoCaso")}
+                >
+                    Info. Casos
+                </button>
+                <button
+                    className="optionsCase"
+                    style={{
+                        backgroundColor:
+                            optionsMenu === "Generar" && "rgb(184, 184, 184)",
+                    }}
+                    onClick={() => setOptionsMenu("Generar")}
+                >
+                    Generar Caso
+                </button>
+                <button
+                    className="optionsCase"
+                    style={{
+                        backgroundColor:
+                            optionsMenu === "Apoyo" && "rgb(184, 184, 184)",
+                    }}
+                    onClick={() => setOptionsMenu("Apoyo")}
+                >
+                    Apoyo
+                </button>
+            </div>
+            {optionsMenu === "Generar" ? (
+                <CaseFormC
+                    selectedBrigade={selectedBrigade}
+                    options={options}
+                    optionsPlace={optionsPlace}
+                    optionsCod={optionsCod}
+                    optionsCategory={optionsCategory}
+                    fillCase={fillCase}
+                    filterOptions1={filterOptions1}
+                    checkFunctionCase={checkFunctionCase}
+                />
+            ) : optionsMenu === "Apoyo" ? (
+                <HelpFormC
+                    selectedBrigade={selectedBrigade}
+                    helpCaseFilter={helpCaseFilter}
+                    fillCase={fillCase}
+                    filterOptions1={filterOptions1}
+                    checkFunctionHelp={checkFunctionHelp}
+                />
+            ) : (
+                <HotActiveCase activeCases={activeCases} />
+            )}
+        </div>
+    );
 }
 
 export default CaseForm;
