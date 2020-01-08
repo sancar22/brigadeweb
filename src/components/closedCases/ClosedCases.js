c043cdfa322704676d17070acf4588a52951b001
unknown <santiagoyacaman@uninorte.edu.co>
2020-01-08 10:56:16 -0500

    fixed /closed page

--- src/components/closedCases/ClosedCases.js
+++ src/components/closedCases/ClosedCases.js
@@ -8,8 +8,6 @@ import "./ClosedCases.css";
 function ClosedCases(props) {
     const allCases = useSelector(state => state.casos);
 
-    let closedCases = allCases.filter(data => data.active === false);
-    console.log(closedCases);
     app.auth().onAuthStateChanged(user => {
         // Para llevarlo a login window si no está conectado
         if (!user) {
@@ -17,49 +15,6 @@ function ClosedCases(props) {
         }
     });
 
-    let displayClosedCases = closedCases.map((item, index) => {
-        return (
-            <tr key={index}>
-                <td>{item.nombre + " " + item.apellido}</td>
-
-                <td>
-                    <div style={{ flexDirection: "column" }}>
-                        <div>Lugar: {item.lugar}</div>
-                        <div>Código: {item.codigo}</div>
-                        <div>Categoría: {item.categoria}</div>
-                        <div>Comienzo: {item.inicioFecha}</div>
-                        <div>Fin: {item.finalFecha}</div>
-                        <div>Tiempo Total: {item.formatTime}</div>
-                    </div>
-                </td>
-                <td>
-                    <div>{item.descripcion}</div>
-                </td>
-                <td>
-                    <div style={{ flexDirection: "column", display: "flex" }}>
-                        {item.bombero && <div>Bombero</div>}
-                        {item.ambulancia && <div>Ambulancia</div>}
-                        {item.camilla && <div>Camilla</div>}
-                        {item.extintor && <div>Extintor</div>}
-                        {item.policia && <div>Policía</div>}
-                        {item.apoyo && <div>Apoyo</div>}
-                    </div>
-                </td>
-                <td>
-                    <div>No disponible por el momento.</div>
-                </td>
-                <td>{item.descBrigadista}</td>
-                <td>
-                    <a href={item.image1} target="_blank">
-                        <img src={item.image1} height="100" width="100" />
-                    </a>
-                    <a href={item.image2} target="_blank">
-                        <img src={item.image2} height="100" width="100" />
-                    </a>
-                </td>
-            </tr>
-        );
-    });
     return (
         <div style={{ overflow: "hidden", height: "100vh", width: "100vw" }}>
             <Navigation />
@@ -74,7 +29,98 @@ function ClosedCases(props) {
                         <th>Proceder</th>
                         <th>Fotos</th>
                     </tr>
-                    {displayClosedCases}
+                    {allCases.length > 0 &&
+                        allCases
+                            .filter(data => data.active === false)
+                            .map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>
+                                            {item.nombre + " " + item.apellido}
+                                        </td>
+
+                                        <td>
+                                            <div
+                                                style={{
+                                                    flexDirection: "column"
+                                                }}
+                                            >
+                                                <div>Lugar: {item.lugar}</div>
+                                                <div>Código: {item.codigo}</div>
+                                                <div>
+                                                    Categoría: {item.categoria}
+                                                </div>
+                                                <div>
+                                                    Comienzo: {item.inicioFecha}
+                                                </div>
+                                                <div>
+                                                    Fin: {item.finalFecha}
+                                                </div>
+                                                <div>
+                                                    Tiempo Total:{" "}
+                                                    {item.formatTime}
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div>{item.descripcion}</div>
+                                        </td>
+                                        <td>
+                                            <div
+                                                style={{
+                                                    flexDirection: "column",
+                                                    display: "flex"
+                                                }}
+                                            >
+                                                {item.bombero && (
+                                                    <div>Bombero</div>
+                                                )}
+                                                {item.ambulancia && (
+                                                    <div>Ambulancia</div>
+                                                )}
+                                                {item.camilla && (
+                                                    <div>Camilla</div>
+                                                )}
+                                                {item.extintor && (
+                                                    <div>Extintor</div>
+                                                )}
+                                                {item.policia && (
+                                                    <div>Policía</div>
+                                                )}
+                                                {item.apoyo && <div>Apoyo</div>}
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div>
+                                                No disponible por el momento.
+                                            </div>
+                                        </td>
+                                        <td>{item.descBrigadista}</td>
+                                        <td>
+                                            <a
+                                                href={item.image1}
+                                                target="_blank"
+                                            >
+                                                <img
+                                                    src={item.image1}
+                                                    height="100"
+                                                    width="100"
+                                                />
+                                            </a>
+                                            <a
+                                                href={item.image2}
+                                                target="_blank"
+                                            >
+                                                <img
+                                                    src={item.image2}
+                                                    height="100"
+                                                    width="100"
+                                                />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
                 </table>
             </div>
         </div>
